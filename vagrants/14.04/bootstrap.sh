99edc5fc6ea807e3e65468eca27e0367a7b1b6e7
Ian A. Mason <iam@csl.sri.com>
2015-11-20 18:55:23 -0800

    fixes to the bootstrap.sh

--- vagrants/14.04/bootstrap.sh
+++ vagrants/14.04/bootstrap.sh
@@ -1,15 +1,17 @@
 #!/usr/bin/env bash
 
-apt-get update
-apt-get install -y emacs24 dbus-x11
-apt-get install -y git subversion wget libprotobuf-dev python-protobuf protobuf-compiler
-apt-get install -y llvm-3.5 libclang-3.5-dev clang-3.5 tree
+sudo apt-get update
+sudo apt-get install -y emacs24 dbus-x11
+sudo apt-get install -y git subversion wget libprotobuf-dev python-protobuf protobuf-compiler
+sudo apt-get install -y llvm-3.5 libclang-3.5-dev clang-3.5 tree
+
 
 mkdir Repositories  && \
     mkdir -p occam_home/bin && \
     cd Repositories &&  \
     git clone https://github.com/ashish-gehani/OCCAM.git && \
     git clone https://github.com/SRI-CSL/whole-program-llvm.git && \
-    . /vagrant/bash_profile  && \
+    cp /vagrant/bash_profile  /home/vagrant/.bash_profile && \
     cd OCCAM && \
     git checkout dos 
+
