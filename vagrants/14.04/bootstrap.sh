1570cbee4c10af2cda0646615da93bb9c2a1e718
Ian A. Mason <iam@csl.sri.com>
2015-11-23 09:39:11 -0800

    merge into my new fork.

--- vagrants/14.04/bootstrap.sh
+++ vagrants/14.04/bootstrap.sh
@@ -7,7 +7,6 @@ sudo apt-get install -y llvm-3.5 libclang-3.5-dev clang-3.5 tree
 
 
 mkdir Repositories  && \
-    mkdir -p occam_home/bin && \
     cd Repositories &&  \
     git clone https://github.com/ashish-gehani/OCCAM.git && \
     git clone https://github.com/SRI-CSL/whole-program-llvm.git && \
