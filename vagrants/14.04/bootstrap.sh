cad95d5b5c933f8dd4d16bc4c99c7c7d005f6026
Ian A. Mason <iam@csl.sri.com>
2015-11-23 17:10:49 +0000

    moving to the SRI-CSL fork.

--- vagrants/14.04/bootstrap.sh
+++ vagrants/14.04/bootstrap.sh
@@ -9,7 +9,7 @@ sudo apt-get install -y llvm-3.5 libclang-3.5-dev clang-3.5 tree
 mkdir Repositories  && \
     mkdir -p occam_home/bin && \
     cd Repositories &&  \
-    git clone https://github.com/ashish-gehani/OCCAM.git && \
+    git clone https://github.com/SRI-CSL/OCCAM.git && \
     git clone https://github.com/SRI-CSL/whole-program-llvm.git && \
     cp /vagrant/bash_profile  /home/vagrant/.bash_profile && \
     .  /vagrant/bash_profile && \
