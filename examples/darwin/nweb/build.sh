10bbdbc5c63e8d8ddc404508065f58ef2911f0af
Ian A. Mason <iam@csl.sri.com>
2015-11-27 12:37:00 -0800

    nweb works on darwin, II.

--- examples/darwin/nweb/build.sh
+++ examples/darwin/nweb/build.sh
@@ -21,14 +21,19 @@ extract-bc nweb
 # Previrutalize
 ${OCCAM_HOME}/bin/occam previrt --work-dir=previrt nweb.manifest
 
-
 # Link link the binary into the current directory
 # (it was created in previrt)
-# rm -f nweb
-#iam ln -s previrt/nweb .
+echo "linking nweb to previrt/nweb"
+rm -f nweb
+ln -s previrt/nweb .
 
 # Now build the non-previrt application
-#iam opt -O3 nweb.bc -o nweb.opt.bc
+echo "building the non-previrt application bitcode"
+opt -O3 nweb.bc -o nweb.opt.bc
 
-#iam llc -filetype=obj -o nweb.opt.o nweb.opt.bc
-#iam clang nweb.opt.o -o nweb-base 
+echo "creating the non-previrt application object file"
+llc -filetype=obj -o nweb.opt.o nweb.opt.bc
+
+echo "producing the non-previrt executable: nweb-base"
+clang nweb.opt.o -o nweb-base 
+
