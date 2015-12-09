31f5d090d54e5d3514d4cfa2dd0b083680a927a6
Ian A. Mason <iam@csl.sri.com>
2015-12-08 18:15:01 -0800

    yices prereq stuff in the vagrant build.

--- vagrants/14.04/bootstrap.sh
+++ vagrants/14.04/bootstrap.sh
@@ -1,9 +1,9 @@
 #!/usr/bin/env bash
 
 sudo apt-get update
-sudo apt-get install -y emacs24 dbus-x11
+sudo apt-get install -y emacs24 dbus-x11 libgmp-dev
 sudo apt-get install -y git subversion wget libprotobuf-dev python-protobuf protobuf-compiler
-sudo apt-get install -y llvm-3.5 libclang-3.5-dev clang-3.5 tree
+sudo apt-get install -y llvm-3.5 libclang-3.5-dev clang-3.5 tree 
 
 
 mkdir Repositories  && \
