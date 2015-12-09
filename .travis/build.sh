39493c4dfd0e693bdd09aa2b4894aa26cceb75ac
Ian A. Mason <iam@csl.sri.com>
2015-12-09 13:52:30 -0800

    Add building hello world to the travis build, III.

--- .travis/build.sh
+++ .travis/build.sh
@@ -5,12 +5,6 @@ set -e
 mkdir -p travis_build/occam
 mkdir -p travis_build/Repositories
 
-export OCCAM_HOME=${BUILD_HOME}/travis_build/occam
-
-export OCCAM_LOGFILE=${BUILD_HOME}/travis_build/.occam.log
-
-export REPOS=${BUILD_HOME}/travis_build/Repositories
-
 ## Ubuntu adds suffixes to the LLVM tools that we rely on.
 #export the suffix	    
 export LLVM_SUFFIX=-3.5
