3d10f01d65da1a42d4669506a443298b1da2aab8
Ian A. Mason <iam@csl.sri.com>
2016-02-05 14:37:03 -0800

    www for apache...

--- vagrants/14.04/bootstrap.sh
+++ vagrants/14.04/bootstrap.sh
@@ -5,8 +5,6 @@ sudo apt-get install -y emacs24 dbus-x11 libgmp-dev
 sudo apt-get install -y git subversion wget libprotobuf-dev python-protobuf protobuf-compiler
 sudo apt-get install -y llvm-3.5 libclang-3.5-dev clang-3.5 tree 
 
-wget http://ftp.gnu.org/gnu/coreutils/coreutils-8.24.tar.xz
-
 mkdir Repositories  && \
     cd Repositories &&  \
     git clone https://github.com/SRI-CSL/OCCAM.git && \
