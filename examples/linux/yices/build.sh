2eb04bdd53a3701e023ceec32129be15a0dac730
Ian A Mason <iam@csl.sri.com>
2016-03-03 01:18:03 +0000

    no strip mystery not quite as mysterious.

--- examples/linux/yices/build.sh
+++ examples/linux/yices/build.sh
@@ -1,5 +1,12 @@
 #!/usr/bin/env bash
 
+WORKDIR=previrt_nostrip
+
+export OCCAM_LOGFILE="${PWD}/${WORKDIR}/occam_log_file.txt"
+export OCCAM_LOGLEVEL=INFO
+
+mkdir -p ${WORKDIR}
+
 # Build the manifest file
 cat > yices_smt2.manifest <<EOF
 { "modules" : ["yices_smt2.bc"]
@@ -7,10 +14,10 @@ cat > yices_smt2.manifest <<EOF
 , "libs"    : ["libpoly.so.bc"]
 , "native-libs" : ["-lgmp"]
 , "search"  : ["/usr/lib", "/usr/local/lib", "/usr/lib/x86_64-linux-gnu/"]
-, "args"    : ["--mcsat"]
 , "name"    : "yices_smt2"
 }
 EOF
+#, "args"    : ["--mcsat"]
 
 # Build the manifest file
 cat > yices_smt2_mcsat.manifest <<EOF
@@ -34,7 +41,20 @@ cat > linked_yices_smt2_mcsat.manifest <<EOF
 }
 EOF
 
+# Previrtualize   [--work-dir=<dir>] [--force] [--no-strip] <manifest>
+${OCCAM_HOME}/bin/occam previrt --no-strip --work-dir=${WORKDIR} yices_smt2.manifest
+
+
+# Build the manifest file
+cat > yices_smt2_release.manifest <<EOF
+{ "modules" : ["yices_smt2_release.bc"]
+, "binary"  : "yices_smt2_release"
+, "libs"    : ["libpoly.so.bc"]
+, "native-libs" : ["-lgmp"]
+, "search"  : ["/usr/lib", "/usr/local/lib", "/usr/lib/x86_64-linux-gnu/"]
+, "name"    : "yices_smt2_release"
+}
+EOF
+
 # Previrtualize
-${OCCAM_HOME}/bin/occam previrt --work-dir=previrt linked_yices_smt2_mcsat.manifest
-
-
+#${OCCAM_HOME}/bin/occam previrt --work-dir=previrt_release yices_smt2.manifest
