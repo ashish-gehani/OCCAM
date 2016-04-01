84fe88e4835e35ba13fa7066027f7d12b4f29a15
Ian A. Mason <iam@csl.sri.com>
2016-04-01 11:44:12 -0700

    Restructuring for the ros build...

---
+++ vagrants/14.04/ros/bootstrap.sh
@@ -0,0 +1,23 @@
+#!/usr/bin/env bash
+
+sudo apt-get update
+sudo apt-get install -y emacs24 dbus-x11 libgmp-dev
+sudo apt-get install -y git subversion wget libprotobuf-dev python-protobuf protobuf-compiler
+sudo apt-get install -y llvm-3.5 libclang-3.5-dev clang-3.5 tree
+
+
+mkdir Repositories  && \
+    cd Repositories &&  \
+    git clone https://github.com/SRI-CSL/OCCAM.git && \
+    git clone https://github.com/SRI-CSL/whole-program-llvm.git && \
+    cp /vagrant/bash_profile  /home/vagrant/.bash_profile && \
+    .  /vagrant/bash_profile && \
+    cd OCCAM && \
+    make && \
+    make install 
+
+#ros bootstrap dependencies
+sudo apt-get install -y python-rosdep python-rosinstall-generator python-wstool python-rosinstall build-essential
+
+
+
