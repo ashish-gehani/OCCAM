dcff9c102d80c339fc3bce8be7bffba82938129e
Ian A Mason <iam@csl.sri.com>
2016-04-20 11:47:04 -0700

    Fxing examples...

--- examples/linux/nweb/build.sh
+++ examples/linux/nweb/build.sh
@@ -1,7 +1,15 @@
 #!/usr/bin/env bash
 
+export OCCAM_LOGFILE=${PWD}/previrt/occam.log
+export OCCAM_LOGLEVEL=INFO
+
+mkdir -f previrt
+
 ROOT=`pwd`/root
 
+OPT=${LLVM_OPT_NAME:-opt}
+LLC=${LLVM_LLC_NAME:-llc}
+CC=${LLVM_CC_NAME:-clang}
 # Build the manifest file
 cat > nweb.manifest <<EOF
 { "modules" : ["nweb.bc"]
@@ -30,10 +38,16 @@ ln -s previrt/nweb .
 
 # Now build the non-previrt application
 echo "building the non-previrt application bitcode"
-${LLVM_OPT_NAME} -O3 nweb.bc -o nweb.opt.bc
+${OPT} -O3 nweb.bc -o nweb.opt.bc
 
 echo "creating the non-previrt application object file"
-${LLVM_LLC_NAME} -filetype=obj -o nweb.opt.o nweb.opt.bc
+${LLC} -filetype=obj -o nweb.opt.o nweb.opt.bc
 
 echo "producing the non-previrt executable: nweb-base"
-${LLVM_CC_NAME} nweb.opt.o -o nweb-base 
+${CC} nweb.opt.o -o nweb-base 
+
+#debugging stuff below:
+for bitcode in previrt/*.bc; do
+    llvm-dis  "$bitcode" &> /dev/null
+done
+
