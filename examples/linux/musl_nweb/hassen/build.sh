283f2f5b2d7e8f2aa527c0a676a14fbf571e8a35
Ian A Mason <iam@csl.sri.com>
2016-06-17 11:59:21 -0700

    Notes on musl libc.

--- examples/linux/musl_nweb/hassen/build.sh
+++ examples/linux/musl_nweb/hassen/build.sh
@@ -8,7 +8,6 @@ llc -filetype=obj static.bc
 MUSL=../../../../../musllvm
 SRC=${MUSL}/src
 
-
 clang -nostdlib static.o ${SRC}/internal/LLVM/syscall.s ${SRC}/setjmp/LLVM/setjmp.s ${SRC}/setjmp/LLVM/longjmp.s ${SRC}/thread/LLVM/clone.s ${SRC}/thread/LLVM/syscall_cp.s ${SRC}/thread/LLVM/__set_thread_area.s ${SRC}/thread/LLVM/__unmapself.s main.s -o static.exe
 
 
