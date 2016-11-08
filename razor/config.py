0b2221536930b20bf05008019593d9ea0686f456
Ian A. Mason <iam@csl.sri.com>
2016-11-07 18:02:18 -0800

    razor gets the occam's razor treatment.

--- razor/config.py
+++ razor/config.py
@@ -1,50 +1,10 @@
 import os, platform, sys
 
-#  All configuration requests should go through 'theConfig' object
-#  It belongs to the class below.
-#  It is created (in config.py) by the genconfig.py script.
-#  The important requests are:
-#
-#  theConfig.getOccamLib()
-#  theConfig.getLogfile()
-#  theConfig.getLLVM(tool)
-#  theConfig.getSTD(tool)
-#
 
 class cfgObj(object):
 
     def  __init__(self, libfile):
         self._occamlib = libfile
-        self._llvm = { 'link'        : 'llvm-link'
-                       , 'as'        : 'llvm-as'
-                       , 'ar'        : 'llvm-ar'
-                       , 'ld'        : 'llvm-ld'
-                       , 'opt'       : 'opt'
-                       , 'clang'     : 'clang'
-                       , 'clang++'   : 'clang++'
-                       , 'clang-cpp' : 'clang-cpp'
-                       , 'nm'        : 'llvm-nm'
-                       , 'llc'       : 'llc'
-                   }
-
-        self._std =  { 'as'          : 'as'
-                       , 'ar'        : 'ar'
-                       , 'nm'        : 'nm'
-                       , 'ld'        : 'ld'
-                       , 'clang'     : 'clang'
-                       , 'clang++'   : 'clang++'
-                       , 'clang-cpp' : 'clang-cpp'
-                       , 'install'   : 'install'
-                       , 'ranlib'    : 'ranlib'
-                       , 'cp'        : 'cp'
-                       , 'mv'        : 'mv'
-                       , 'cpp'       : 'cpp'
-                       , 'file'      : 'file'
-                       , 'chmod'     : 'chmod'
-                       , 'ln'        : 'ln'
-                       , 'rm'        : 'rm'
-                       , 'unlink'    : 'unlink'
-                   }
         
         self._env = { 'clang'        :  'LLVM_CC_NAME'
                       , 'clang++'    :  'LLVM_CXX_NAME'  
@@ -73,16 +33,7 @@ class cfgObj(object):
             logfile = '/tmp/occam.log'
         return logfile
 
-    def getStdTool(self, tool):
-        candidate = tool
-        if self._std.has_key(tool):
-            candidate = self._std[tool]
-        return self.env_version(tool)
-
     def getLLVMTool(self, tool):
-        candidate = tool
-        if self._llvm.has_key(tool):
-            candidate = self._llvm[tool]
         return self.env_version(tool)
 
 def getOccamLibPath():
@@ -107,9 +58,6 @@ def getOccamLib():
 
 def getLogfile():
     return theConfig.getLogfile()
-
-def getStdTool(tool):
-    return theConfig.getStdTool(tool)
 
 def getLLVMTool(tool):
     return theConfig.getLLVMTool(tool)
