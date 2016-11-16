1621030a8d3a77079d9e6989d4a23c1cc4e2853a
Ian A. Mason <iam@csl.sri.com>
2016-11-16 15:43:43 -0800

    version bump.

--- razor/version.py
+++ razor/version.py
@@ -3,7 +3,16 @@ Freeping Creaturism:
 
 This is the all important version number used by pip.
 
+Version History:
+
+1.0.0    - 11/15/2016 initial birth as a pip package.
+
+1.0.1    - 11/16/2016 decided to enforce the new manifesto, now that it is documented.
+
+
+
+
 """
 
 
-razor_version = '1.0.0'
+razor_version = '1.0.1'
