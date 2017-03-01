7c47c37f094d9c43c1d61ee10fbfa08667e63c8c
caballa <navasjorgea@gmail.com>
2017-02-28 16:08:03 -0800

    Added a new test case for an extern global variable

--- examples/multiple/main.c
+++ examples/multiple/main.c
@@ -5,6 +5,7 @@
 #include "library.h"
 
 int global = 666;
+static void * p;
 
 int main(int argc, char* argv[]){
   int retval = 0;
@@ -19,11 +20,17 @@ int main(int argc, char* argv[]){
     retval =
       libcall_int(1, atol(argv[1]))          *           // 2
       libcall_int(2, atol(argv[1]))          *           // 3
+      libcall_int(global, atol(argv[1]))     *           // 3
+      // no specialized (global2 is external)
+      libcall_int(global2, atol(argv[1]))    *           // 3            
       libcall_float(strlen(argv[1]), 0.5F)   *           // 7
       libcall_double(strlen(argv[1]), 0.75)  *           // 17
       libcall_null_pointer(4, NULL)          *           // 5
       libcall_string(5, "Z")                 *           // 11
-      libcall_global_pointer(6, &libcall_global_pointer); //13
+      // no specialized 
+      libcall_global_pointer(strlen(argv[1]), &libcall_global_pointer); //13
+      // no specialized
+      libcall_global_pointer(strlen(argv[1]), p); //13    
 
   } else {
 
