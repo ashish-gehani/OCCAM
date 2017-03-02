346d9edd02b85d140f170ad34714254f649a5586
Ian A. Mason <iam@csl.sri.com>
2017-03-02 10:51:50 -0800

    Learning advanced travis-fu

--- .travis/build.sh
+++ .travis/build.sh
@@ -2,21 +2,6 @@
 # Make sure we exit if there is a failure
 set -e
 
-sudo cat <<EOF >> /etc/apt/sources.list
-deb http://apt.llvm.org/trusty/ llvm-toolchain-trusty main
-deb-src http://apt.llvm.org/trusty/ llvm-toolchain-trusty main
-# 3.9
-deb http://apt.llvm.org/trusty/ llvm-toolchain-trusty-3.9 main
-deb-src http://apt.llvm.org/trusty/ llvm-toolchain-trusty-3.9 main
-# 4.0
-deb http://apt.llvm.org/trusty/ llvm-toolchain-trusty-4.0 main
-deb-src http://apt.llvm.org/trusty/ llvm-toolchain-trusty-4.0 main
-EOF
-
-wget -O - http://apt.llvm.org/llvm-snapshot.gpg.key|sudo apt-key add -
-sudo apt-get update
-sudo apt-get install clang-3.9 lldb-3.9
-
 mkdir -p travis_build/occam
 mkdir -p travis_build/Repositories
 
