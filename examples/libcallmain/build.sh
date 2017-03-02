a4065b7aa6b29be52fd93d60999c74b2ed703192
caballa <navasjorgea@gmail.com>
2017-03-02 10:30:07 -0800

    Added example from LTO LLVM website where a library function is defined in main. We do not currently support this scenario.

---
+++ examples/libcallmain/build.sh
@@ -0,0 +1,44 @@
+#!/usr/bin/env bash
+
+LIBRARY='library'
+
+
+unamestr=`uname`
+if [[ "$unamestr" == 'Linux' ]]; then
+   LIBRARY='library.so'
+elif [[ "$unamestr" == 'Darwin' ]]; then
+   LIBRARY='library.dylib'
+fi
+
+
+# Build the manifest file  (FIXME: dylib not good for linux)
+cat > simple.manifest <<EOF
+{ "main" : "main.bc"
+, "binary"  : "main"
+, "modules"    : ["${LIBRARY}.bc"]
+, "native_libs" : []
+, "args"    : []
+, "name"    : "main"
+}
+EOF
+
+#make the bitcode
+CC=wllvm make 
+extract-bc main
+extract-bc ${LIBRARY}
+
+
+export OCCAM_LOGLEVEL=INFO
+export OCCAM_LOGFILE=${PWD}/slash/occam.log
+export PATH=${LLVM_HOME}/bin:${PATH}
+
+slash --work-dir=slash simple.manifest
+
+cp slash/main main_slash
+
+#debugging stuff below:
+for bitcode in slash/*.bc; do
+    llvm-dis  "$bitcode" &> /dev/null
+done
+
+exit 0
