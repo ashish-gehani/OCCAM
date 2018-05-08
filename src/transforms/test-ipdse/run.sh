67a87b6370b042fa4bf20875e621df3e15634a4f
caballa <navasjorgea@gmail.com>
2018-05-07 18:20:51 -0700

    Tests for IPDSE transformation

---
+++ src/transforms/test-ipdse/run.sh
@@ -0,0 +1,37 @@
+#!/bin/bash
+
+usage () {
+    echo "Usage: $0 prog.c"
+}
+
+if [ $# -ne 1 ]
+then
+    usage 
+    exit 1
+fi
+
+
+CLANG=${LLVM_HOME}/bin/clang
+OPT=${LLVM_HOME}/bin/opt
+LIBS="-load=${OCCAM_HOME}/lib/libSeaDsa.dylib -load=${OCCAM_HOME}/lib/libprevirt.dylib"
+
+dirpath=$(dirname "$1")
+filename=$(basename -- "$1")
+extension="${filename##*.}"
+filename="${filename%.*}"
+
+
+IN=$1
+OUT=$dirpath/$filename.bc
+echo "$CLANG -c -emit-llvm -O0 -Xclang -disable-O0-optnone $IN -o $OUT"
+$CLANG -c -emit-llvm -O0 -Xclang -disable-O0-optnone $IN -o $OUT
+
+IN=$OUT
+OUT=$dirpath/$filename.memssa.bc
+echo "$OPT $LIBS -mem2reg --lower-gv-init -memory-ssa -mem2reg $IN  -o $OUT"
+$OPT $LIBS -mem2reg --lower-gv-init -memory-ssa -mem2reg $IN  -o $OUT
+
+IN=$OUT
+OUT=$dirpath/$filename.o.bc
+echo "$OPT $LIBS -ip-dse -strip-memory-ssa-inst -globaldce -ipsccp -dce -globaldce $IN -o $OUT"
+$OPT $LIBS -ip-dse -strip-memory-ssa-inst -globaldce -ipsccp -dce -globaldce $IN -o $OUT
