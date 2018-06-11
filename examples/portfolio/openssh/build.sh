c160a725c86ff13635c0a70ade686f10e55e28f4
Ian A. Mason <iam@csl.sri.com>
2018-06-11 11:53:22 -0700

    improvements to the build script.

--- examples/portfolio/openssh/build.sh
+++ examples/portfolio/openssh/build.sh
@@ -3,12 +3,29 @@
 # Make sure we exit if there is a failure
 set -e
 
+# Make sure we exit if there is a failure
+set -e
+
+#check that the require dependencies are built
+declare -a bitcode=("ssh.bc" "libcrypto.a.bc" "libz.a.bc")
+
+for bc in "${bitcode[@]}"
+do
+    if [ -a  "$bc" ]
+    then
+        echo "Found $bc"
+    else
+        echo "Error: $bc not found. Try \"make\"."
+        exit 1
+    fi
+done
+
 
 echo "Linking ssh_from_bc"
 clang++ ssh.bc libcrypto.a.bc libz.a.bc -ldl -lresolv -o ssh_from_bc
 
-echo "Linking sshd_from_bc"
-clang++ sshd.bc libcrypto.a.bc libz.a.bc -ldl -lresolv -o sshd_from_bc
+#echo "Linking sshd_from_bc"
+#clang++ sshd.bc libcrypto.a.bc libz.a.bc -ldl -lresolv -o sshd_from_bc
 
 export OCCAM_LOGLEVEL=INFO
 export OCCAM_LOGFILE=${PWD}/slash/occam.log
@@ -30,4 +47,4 @@ EOF
 # Previrtualize
 slash --stats --devirt --work-dir=slash ssh.manifest
 
-cp slash/ssh_slashed .
+cp ./slash/ssh_slashed .
