d5f64004d0c18cc945f74e6fd3325678be1c8abc
Ian A. Mason <iam@csl.sri.com>
2018-08-01 11:49:09 -0700

    Improved the debugging facilities available: --verbose, --debug, --debug-manager=<type>, debug-pass=<tag>. Do 'slash --help' for more details. Also made outout to logfile real time rather than 'after the subprocess exits', II

---
+++ razor/echo.py
@@ -0,0 +1,58 @@
+"""
+ OCCAM
+
+ Copyright (c) 2011-2017, SRI International
+
+  All rights reserved.
+
+ Redistribution and use in source and binary forms, with or without
+ modification, are permitted provided that the following conditions are met:
+
+ * Redistributions of source code must retain the above copyright notice, this
+   list of conditions and the following disclaimer.
+
+ * Redistributions in binary form must reproduce the above copyright notice,
+   this list of conditions and the following disclaimer in the documentation
+   and/or other materials provided with the distribution.
+
+ * Neither the name of SRI International nor the names of its contributors may
+   be used to endorse or promote products derived from this software without
+   specific prior written permission.
+
+ THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+ AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+ IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+ DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE
+ FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+ DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
+ SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+ CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
+ OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+ OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+"""
+
+from threading import Thread
+import logging
+
+class Echo:
+
+    def __init__(self, stream, logger):
+        """Echos the stream to the logger.
+        """
+        self.stream = stream
+        self.logger = logger
+
+
+        def thread_main(stream, logger):
+            while True:
+                line = stream.readline()
+                if line == '':
+                    return
+                else:
+                    logger.log(logging.INFO, line.rstrip())
+
+        self.thread = Thread(target = thread_main, args = (self.stream, self.logger))
+
+        self.thread.daemon = True
+
+        self.thread.start()
