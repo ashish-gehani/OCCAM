eb7fd3fb892106a1269896338ab8f90b6f18ca64
caballa <navasjorgea@gmail.com>
2019-01-26 15:14:59 -0800

    Modified scripts to pass user options

--- examples/portfolio/openssh/build.sh
+++ examples/portfolio/openssh/build.sh
@@ -3,6 +3,54 @@
 # Make sure we exit if there is a failure
 set -e
 
+function usage() {
+    echo "Usage: build.sh [--inter-spec VAL] [--intra-spec VAL] [--link dynamic|static] [--help]"
+    echo "       VAL=none|aggressive|nonrec-aggressive"
+}
+
+#default values
+LINK="dynamic"
+INTER_SPEC="none"
+INTRA_SPEC="none"
+
+POSITIONAL=()
+while [[ $# -gt 0 ]]
+do
+key="$1"
+case $key in
+    -link|--link)
+	LINK="$2"
+	shift # past argument
+	shift # past value
+	;;    
+    -inter-spec|--inter-spec)
+	INTER_SPEC="$2"
+	shift # past argument
+	shift # past value
+	;;
+    -intra-spec|--intra-spec)
+	INTRA_SPEC="$2"
+	shift # past argument
+	shift # past value
+	;;
+    -help|--help)
+	usage
+	exit 0
+	;;
+    *)    # unknown option
+	POSITIONAL+=("$1") # save it in an array for later
+	shift # past argument
+	;;
+esac
+done
+set -- "${POSITIONAL[@]}" # restore positional parameters
+
+# Check user inputs
+if [[ "${LINK}" != "dynamic"  &&  "${LINK}" != "static" ]]; then
+    echo "error: link can only be dynamic or static"
+    exit 1
+fi    
+
 #check that the required dependencies are built
 declare -a bitcode=("ssh.bc" "libcrypto.a.bc" "libz.a.bc")
 
@@ -18,19 +66,22 @@ do
 done
 
 
-echo "Linking ssh_from_bc"
-clang++ ssh.bc libcrypto.a.bc libz.a.bc -ldl -lresolv -o ssh_from_bc
+#echo "Linking ssh_from_bc"
+#clang++ ssh.bc libcrypto.a.bc libz.a.bc -ldl -lresolv -o ssh_from_bc
 
 #echo "Linking sshd_from_bc"
 #clang++ sshd.bc libcrypto.a.bc libz.a.bc -ldl -lresolv -o sshd_from_bc
 
-export OCCAM_LOGLEVEL=INFO
-export OCCAM_LOGFILE=${PWD}/slash/occam.log
+# OCCAM with program and libraries dynamically linked
+function dynamic_link() {
 
-rm -rf slash ssh_slashed
+    export OCCAM_LOGLEVEL=INFO
+    export OCCAM_LOGFILE=${PWD}/slash/occam.log
 
-# Build the manifest file
-cat > ssh.manifest <<EOF
+    rm -rf slash ssh_slashed
+
+    # Build the manifest file
+    cat > ssh.manifest <<EOF
 { "main" : "ssh.bc"
 , "binary"  : "ssh_slashed"
 , "modules"    : ["libcrypto.a.bc", "libz.a.bc"]
@@ -40,10 +91,57 @@ cat > ssh.manifest <<EOF
 }
 EOF
 
+    # additional debug flags:
+    #  --debug-pass=sroa --verbose --debug-manager=Structure
+    SLASH_OPTS="--inter-spec-policy=${INTER_SPEC} --intra-spec-policy=${INTRA_SPEC}"
+    echo "============================================================"
+    echo "Running httpd with dynamic libraries crypto, z"
+    echo "slash options ${SLASH_OPTS}"
+    echo "============================================================"
+    slash ${SLASH_OPTS} --stats --devirt --work-dir=slash ssh.manifest
+    status=$?
+    if [ $status -ne 0 ]
+    then
+	echo "Something failed while running slash"
+	exit 1
+    fi     
+    cp ./slash/ssh_slashed .
+}
 
-# Previrtualize
-# additional debug flags:
-#  --debug-pass=sroa --verbose --debug-manager=Structure
-slash  --stats --devirt --work-dir=slash ssh.manifest
+# OCCAM with program and libraries statically linked
+function static_link() {
+    llvm-link ssh.bc libcrypto.a.bc libz.a.bc -o linked_ssh.bc
+    # Build the manifest file
+    cat > linked_ssh.manifest <<EOF
+{ "main" : "linked_ssh.bc"
+, "binary"  : "ssh_static_slashed"
+, "modules"    : []
+, "native_libs" : ["-ldl", "-lresolv"]
+, "name"    : "ssh"
+, "constraints" : ["1", "-Y", "-4"]
+}
+EOF
 
-cp ./slash/ssh_slashed .
+    # additional debug flags:
+    #  --debug-pass=sroa --verbose --debug-manager=Structure
+    SLASH_OPTS="--inter-spec-policy=${INTER_SPEC} --intra-spec-policy=${INTRA_SPEC}"
+    echo "============================================================"
+    echo "Running httpd with libraries crypto, z statically linked"
+    echo "slash options ${SLASH_OPTS}"
+    echo "============================================================"
+    slash ${SLASH_OPTS} --stats --devirt --work-dir=slash linked_ssh.manifest
+    status=$?
+    if [ $status -ne 0 ]
+    then
+	echo "Something failed while running slash"
+	exit 1
+    fi     
+    cp ./slash/ssh_static_slashed .
+}
+
+if [ "${LINK}" == "dynamic" ]; then
+    dynamic_link 
+else
+    static_link
+fi    
+
