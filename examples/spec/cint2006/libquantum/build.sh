ca25000154959d122ce0b9553a0399c57a26797c
caballa <navasjorgea@gmail.com>
2019-01-28 14:55:50 -0800

    Updated scripts

--- examples/spec/cint2006/libquantum/build.sh
+++ examples/spec/cint2006/libquantum/build.sh
@@ -3,6 +3,42 @@
 # Make sure we exit if there is a failure
 set -e
 
+function usage() {
+    echo "Usage: build.sh [--inter-spec VAL] [--intra-spec VAL] [--help]"
+    echo "       VAL=none|aggressive|nonrec-aggressive"
+}
+
+#default values
+INTER_SPEC="none"
+INTRA_SPEC="none"
+
+POSITIONAL=()
+while [[ $# -gt 0 ]]
+do
+key="$1"
+case $key in
+    -inter-spec|--inter-spec)
+	INTER_SPEC="$2"
+	shift # past argument
+	shift # past value
+	;;
+    -intra-spec|--intra-spec)
+	INTRA_SPEC="$2"
+	shift # past argument
+	shift # past value
+	;;
+    -help|--help)
+	usage
+	exit 0
+	;;
+    *)    # unknown option
+	POSITIONAL+=("$1") # save it in an array for later
+	shift # past argument
+	;;
+esac
+done
+set -- "${POSITIONAL[@]}" # restore positional parameters
+
 #check that the require dependencies are built
 declare -a bitcode=("libquantum.bc")
 
@@ -35,5 +71,12 @@ EOF
 
 # Run OCCAM
 cp ./libquantum ./libquantum_orig
-slash --stats --devirt --work-dir=slash libquantum.manifest
+
+SLASH_OPTS="--inter-spec-policy=${INTER_SPEC} --intra-spec-policy=${INTRA_SPEC}"
+echo "============================================================"
+echo "Running with options ${SLASH_OPTS}"
+echo "============================================================"
+slash ${SLASH_OPTS}  \
+      --no-strip --stats --devirt --work-dir=slash libquantum.manifest
+
 cp ./slash/libquantum_slashed .
