8447d1ac5b58ee6008f14beb8dd88caee588f014
caballa <navasjorgea@gmail.com>
2019-03-28 08:50:48 -0700

    Add utility to inline specific functions

---
+++ include/utils/Inliner.h
@@ -0,0 +1,17 @@
+#pragma once
+
+namespace llvm {
+  class Module;
+  class Function;
+  template<typename PtrType, unsigned SmallSize> class SmallPtrSet;
+}
+
+namespace previrt {
+namespace utils {
+
+  // Force to inline a function only if it belongs to inlined_functions.
+  bool inlineOnly(llvm::Module& M, 
+		  const llvm::SmallPtrSet<llvm::Function*, 8>& inline_functions);
+  
+}
+}
