128e414d60ae4b5dcf226044767739e1fc61c0ab
caballa <navasjorgea@gmail.com>
2019-04-30 15:18:59 -0700

    Forgot to modify tests

--- examples/multiple/build.sh
+++ examples/multiple/build.sh
@@ -32,7 +32,8 @@ export OCCAM_LOGLEVEL=INFO
 export OCCAM_LOGFILE=${PWD}/slash/occam.log
 export PATH=${LLVM_HOME}/bin:${PATH}
 
-slash --work-dir=slash multiple.manifest
+slash --intra-spec-policy=nonrec-aggressive --inter-spec-policy=nonrec-aggressive \
+      --work-dir=slash multiple.manifest
 
 cp slash/main main_slash
 
