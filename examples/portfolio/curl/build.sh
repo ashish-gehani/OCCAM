be77908a10cbba2dcc0def7c4eeb003f82d39133
Ian A. Mason <iam@csl.sri.com>
2019-04-30 15:31:24 -0700

    curl example now works out of box on linux (libtool and clang++ issues on OSX)

--- examples/portfolio/curl/build.sh
+++ examples/portfolio/curl/build.sh
@@ -3,5 +3,8 @@
 export OCCAM_LOGLEVEL=INFO
 export OCCAM_LOGFILE=${PWD}/slash_simple/occam.log
 
-slash  --stats --work-dir=slash_simple --amalgamate=slash_simple/amalgamation.bc curl.manifest.previrt
-cp slash_simple/curl curl_slash_simple
+#slash  --stats --work-dir=slash_simple --amalgamate=slash_simple/amalgamation.bc curl.manifest.previrt
+#cp slash_simple/curl curl_slash_simple
+
+slash  --stats --work-dir=slash_specialized --amalgamate=slash_specialized/amalgamation.bc curl.manifest.specialized
+cp slash_specialized/curl curl_slash_specialized
