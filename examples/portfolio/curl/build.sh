f817485cff4b0857aa07ca00bb2d1104608cc0cc
caballa <navasjorgea@gmail.com>
2019-04-29 20:20:50 -0400

    Add linker flags to curl example

--- examples/portfolio/curl/build.sh
+++ examples/portfolio/curl/build.sh
@@ -1,9 +1,7 @@
 #!/usr/bin/env bash
 
-
-
 export OCCAM_LOGLEVEL=INFO
 export OCCAM_LOGFILE=${PWD}/slash_simple/occam.log
 
-slash --work-dir=slash_simple --amalgamate=slash_simple/amalgamation.bc curl.manifest.previrt
+slash  --stats --work-dir=slash_simple --amalgamate=slash_simple/amalgamation.bc curl.manifest.previrt
 cp slash_simple/curl curl_slash_simple
