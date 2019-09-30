5ea6fc9aa3df687934827d707e6ff24119ea4d01
caballa <navasjorgea@gmail.com>
2019-09-29 19:25:49 -0700

    Added comments

--- examples/funcs/5/main.c
+++ examples/funcs/5/main.c
@@ -1,6 +1,15 @@
 #include <stdio.h>
 #include "library.h"
 
+/*
+  Similar to funcs/4 but init_callbacks and equal_str are defined in a
+  library. This example is very challenging and it's currently beyond
+  what OCCAM can do. First, we cannot reason about string contents so
+  we won't be able to tell whether argv[1] is "one", "two" or
+  "three". Second, the array cb is populated in the library. OCCAM's
+  inter-module specialization cannot propagate the contents of cb so
+  we won't know which functions are being called.
+ */
 extern void init_callbacks (int (**cb)(int, int));
 extern int equal_str (const char* s1, const char* s2);
 
