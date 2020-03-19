f9d3b688c081a939a6bc1ffa3ae763fd8d6d8bfe
Jorge <jorge.navas@sri.com>
2020-03-19 14:08:23 -0700

    use sea-dsa instead of llvm-dsa

--- examples/portfolio/memcached/build.sh
+++ examples/portfolio/memcached/build.sh
@@ -5,14 +5,14 @@ set -e
 
 function usage() {
     echo "Usage: $0 [--with-musllvm] [--disable-inlining] [--ipdse] [--ai-dce] [--devirt VAL1] [--inter-spec VAL2] [--intra-spec VAL2] [--help]"
-    echo "       VAL1=none|dsa|cha_dsa"    
-    echo "       VAL2=none|aggressive|nonrec-aggressive"
+    echo "       VAL1=none|sea_dsa (default)|cha_dsa"    
+    echo "       VAL2=none (default)|aggressive|nonrec-aggressive"
 }
 
 #default values
 INTER_SPEC="none"
 INTRA_SPEC="none"
-DEVIRT="dsa"
+DEVIRT="sea_dsa"
 OPT_OPTIONS=""
 USE_MUSLLVM="false"
 
