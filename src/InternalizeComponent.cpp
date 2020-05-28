ashish-gehani/OCCAM

commit 81cfe55c5788361cef318ef89b67b39505f44bb8

refactor(ComponentInterface): make private class fields

src/InternalizeComponent.cpp
@@ -190,8 +190,7 @@ bool MinimizeComponent(Module &M, const ComponentInterface &I) {
       continue;
     }
 
-    if (I.references.find(alias.getName()) == I.references.end() &&
-        alias.use_empty()) {
+    if (I.hasReference(alias.getName()) && alias.use_empty()) {
       errs() << "Remove unused alias " << alias.getName() << "\n";
       unusedAliases.push_back(&alias);
     } else {
@@ -219,8 +218,8 @@ bool MinimizeComponent(Module &M, const ComponentInterface &I) {
         // f is discardable if unused in other compilation units
         isDiscardableIfUnusedExternally(f.getLinkage()) &&
         // unused in other compilation units
-        I.calls.find(f.getName()) == I.calls.end() &&
-        I.references.find(f.getName()) == I.references.end() &&
+        I.hasCall(f.getName()) &&
+        I.hasReference(f.getName()) &&
         // there is no an alias to f that we want to keep
         !keepAliasees.count(&f)) {
 
@@ -256,7 +255,7 @@ bool MinimizeComponent(Module &M, const ComponentInterface &I) {
 
     if (gv.hasInitializer() &&
         // global is unused
-        I.references.find(gv.getName()) == I.references.end() &&
+        I.hasReference(gv.getName()) &&
         isDiscardableIfUnusedExternally(gv.getLinkage()) &&
         // there is no an alias to f that we want to keep
         !keepAliasees.count(&gv)) {

include/Interfaces.h
@@ -113,16 +113,14 @@ public:
   FunctionIterator begin() const;
   FunctionIterator end() const;
 
-  // Find function
-  //FunctionIterator find(llvm::StringRef) const;
-
   // iteration over the calls to a function
   CallIterator call_begin(llvm::StringRef) const;
   CallIterator call_end(llvm::StringRef) const;
 
-  // CallIterator call_begin(FunctionIterator) const;
-  // CallIterator call_end(FunctionIterator) const;
-
+  bool hasReference(llvm::StringRef ref) const { return references.count(ref) > 0;}
+  
+  bool hasCall(llvm::StringRef ref) const { return calls.find(ref) != calls.end();}
+  
   bool readFromFile(const std::string &filename);
 
   FRIEND_SERIALIZERS(ComponentInterface, proto::ComponentInterface)

src/GatherInterface.cpp
@@ -155,12 +155,9 @@ public:
 
     if (!GatherInterfaceEntry.empty()) {
       ComponentInterface ci;
-      for (cl::list<std::string>::const_iterator
-               i = GatherInterfaceEntry.begin(),
-               e = GatherInterfaceEntry.end();
-           i != e; ++i) {
-        errs() << "Reading interface from '" << *i << "'...";
-        if (ci.readFromFile(*i)) {
+      for (auto interfaceName: GatherInterfaceEntry) {
+        errs() << "Reading interface from '" << interfaceName << "'...";
+        if (ci.readFromFile(interfaceName)) {
           errs() << "success\n";
         } else {
           errs() << "failed\n";
@@ -168,9 +165,8 @@ public:
       }
       // errs() << "Searching for external symbols starting from "
       //       << "entries given by the interfaces of other modules:\n";
-      for (ComponentInterface::FunctionIterator i = ci.begin(), e = ci.end();
-           i != e; ++i) {
-        if (Function *f = M.getFunction(i->first())) {
+      for (auto FH: llvm::make_range(ci.begin(), ci.end())) {
+        if (Function *f = M.getFunction(FH)) {
           // errs() << "\tAdded " << f->getName() << "into the queue.\n";
           queue.push_back(cg.getOrInsertFunction(f));
         }

src/InterSpecializer.cpp
@@ -131,20 +131,16 @@ static bool SpecializeComponent(Module &M, ComponentInterfaceTransform &T,
   // TODO: What needs to be done?
   // - Should try to handle strings & arrays
   // Iterate through all functions in the interface of T
-  for (ComponentInterface::FunctionIterator ff = I.begin(), fe = I.end();
-       ff != fe; ++ff) {
-    StringRef name = ff->first();
-    Function *func = resolveFunction(M, name);
-
+  for (auto fName: llvm::make_range(I.begin(), I.end())) {
+    Function *func = resolveFunction(M, fName);
     if (!func || func->isDeclaration()) {
       continue;
     }
 
     // Now iterate through all calls to func in the interface of T
-    for (ComponentInterface::CallIterator cc = I.call_begin(name),
-                                          ce = I.call_end(name);
-         cc != ce; ++cc) {
-      const CallInfo *const call = *cc;
+    for (const CallInfo* call: llvm::make_range(I.call_begin(fName),
+						I.call_end(fName))) {
+      
       const unsigned arg_count = call->args.size();
       if (func->isVarArg()) {
         continue;
@@ -202,9 +198,9 @@ static bool SpecializeComponent(Module &M, ComponentInterfaceTransform &T,
       }
       specialized_func->setLinkage(GlobalValue::ExternalLinkage);
       FunctionHandle rewriteTo = specialized_func->getName();
-      T.rewrite(name, call, rewriteTo, argPerm);
+      T.rewrite(fName, call, rewriteTo, argPerm);
       to_add.push_back(specialized_func);
-      errs() << "Specialized  " << name << " to " << rewriteTo << "\n";
+      errs() << "Specialized  " << fName << " to " << rewriteTo << "\n";
 
 #if 0
 	for (unsigned i = 0; i < arg_count; i++) {
@@ -239,11 +235,9 @@ public:
   InterSpecializerPass() : ModulePass(ID) {
 
     errs() << "InterSpecializerPass():\n";
-    for (cl::list<std::string>::const_iterator b = SpecCompIn.begin(),
-                                               e = SpecCompIn.end();
-         b != e; ++b) {
-      errs() << "Reading file '" << *b << "'...";
-      if (transform.readInterfaceFromFile(*b)) {
+    for (auto fileName: SpecCompIn) {
+      errs() << "Reading file '" << fileName << "'...";
+      if (transform.readInterfaceFromFile(fileName)) {
         errs() << "success\n";
       } else {
         errs() << "failed\n";
@@ -253,7 +247,7 @@ public:
     errs() << "Done reading.\n";
     if (transform.hasInterface()) {
       const ComponentInterface &interface = transform.getInterface();
-      errs() << interface.calls.size() << " calls\n";
+      errs() << std::distance(interface.begin(), interface.end());
     } else {
       errs() << "No interfaces read.\n";
     }

src/Interfaces.cpp
@@ -249,11 +249,6 @@ ComponentInterface::FunctionIterator ComponentInterface::end() const {
   return FunctionIterator(calls.end());
 }
 
-// ComponentInterface::FunctionIterator
-// ComponentInterface::find(StringRef key) const {
-//   return this->calls.find(key);
-// }
-
 ComponentInterface::CallIterator
 ComponentInterface::call_begin(StringRef n) const {
   
@@ -268,15 +263,6 @@ ComponentInterface::call_end(StringRef n) const {
   assert(it != this->calls.end());
   return it->second.end();
 }
-
-// ComponentInterface::CallIterator
-// ComponentInterface::call_begin(FunctionIterator i) const {
-//   return i->second.begin();
-// }
-// ComponentInterface::CallIterator
-// ComponentInterface::call_end(FunctionIterator i) const {
-//   return i->second.end();
-// }
 
 template <>
 void codeInto<ComponentInterface, proto::ComponentInterface>(

commit 9c347866686ea06f6c1a3f4e07cf417691bea536

fix(InternalizeComponent): bug introduced in 81cfe55

src/InternalizeComponent.cpp
@@ -190,7 +190,7 @@ bool MinimizeComponent(Module &M, const ComponentInterface &I) {
       continue;
     }
 
-    if (I.hasReference(alias.getName()) && alias.use_empty()) {
+    if (!I.hasReference(alias.getName()) && alias.use_empty()) {
       errs() << "Remove unused alias " << alias.getName() << "\n";
       unusedAliases.push_back(&alias);
     } else {
@@ -218,8 +218,8 @@ bool MinimizeComponent(Module &M, const ComponentInterface &I) {
         // f is discardable if unused in other compilation units
         isDiscardableIfUnusedExternally(f.getLinkage()) &&
         // unused in other compilation units
-        I.hasCall(f.getName()) &&
-        I.hasReference(f.getName()) &&
+        !I.hasCall(f.getName()) &&
+        !I.hasReference(f.getName()) &&
         // there is no an alias to f that we want to keep
         !keepAliasees.count(&f)) {
 
@@ -255,7 +255,7 @@ bool MinimizeComponent(Module &M, const ComponentInterface &I) {
 
     if (gv.hasInitializer() &&
         // global is unused
-        I.hasReference(gv.getName()) &&
+        !I.hasReference(gv.getName()) &&
         isDiscardableIfUnusedExternally(gv.getLinkage()) &&
         // there is no an alias to f that we want to keep
         !keepAliasees.count(&gv)) {