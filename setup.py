1bb78f54c69ae320beb0bacb823772e3b24b4ce9
Ian A Mason <iam@csl.sri.com>
2020-08-06 16:07:58 -0700

    Update setup.py

--- setup.py
+++ setup.py
@@ -47,10 +47,6 @@ setup(
         'Topic :: System :: Distributed Computing',
         'License :: OSI Approved :: MIT License',
         'Programming Language :: Python',
-        'Programming Language :: Python :: 2',
-        'Programming Language :: Python :: 2.7',
-        'Programming Language :: Python :: 2.7',
         'Programming Language :: Python :: 3',
-
     ],
 )
