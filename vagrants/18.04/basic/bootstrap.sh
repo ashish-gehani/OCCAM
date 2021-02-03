75953a12e6613e0bb073e106b433f7f99dd537b3
Ashish Gehani <ashish.gehani@sri.com>
2021-02-02 21:34:04 -0800

    Untested 18.04 Vagrant

---
+++ vagrants/18.04/basic/bootstrap.sh
@@ -0,0 +1,30 @@
+#!/usr/bin/env bash
+
+# Install GLLVM / OCCAM dependencies
+sudo apt-get update
+sudo apt-get install -y clang-10 clang-format-10 cmake git golang-go
+sudo apt-get install -y libboost-dev libgmp-dev libprotobuf-dev llvm-10 
+sudo apt-get install -y protobuf-compiler python python-pip python-protobuf
+sudo apt-get install -y software-properties-common wget
+pip install setuptools --upgrade
+pip install wheel
+pip install protobuf
+pip install lit
+
+# Download OCCAM
+git clone --recurse-submodules https://github.com/SRI-CSL/OCCAM.git occam
+
+# Set environment variables
+cp occam/vagrants/18.04/basic/bash_profile $HOME/.bash_profile
+. .bash_profile
+
+# Install GLLVM
+mkdir "$GOPATH"
+cd "$GOPATH"
+go get github.com/SRI-CSL/gllvm/cmd/...
+
+# Build OCCAM
+cd $OCCAM_HOME
+make
+make install
+# make test # <-- Currently fails
